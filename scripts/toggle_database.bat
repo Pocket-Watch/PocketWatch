ed5edd30f30b123b3003384bf1ccedb9f8be0e5b
FriskIsGit <friskisgit@users.noreply.github.com>
2025-04-07 20:24:23 +0200

    Db script: detached pg_ctl, fixed errorlevel matching

--- scripts/toggle_database.bat
+++ scripts/toggle_database.bat
@@ -5,13 +5,12 @@ set "scripts_root=%~dp0"
 cd "%scripts_root%.."
 
 REM Check if PostgreSQL is ready
-pg_isready >nul 2>&1
-if "%errorlevel%" equ 0 (
+pg_isready > NUL 2>&1
+if %errorlevel% equ 0 (
     echo Stopping the database.
-    pg_ctl -D database/ -l database/logfile.txt stop
+    pg_ctl -D database/ -l database/logfile.txt stop > NUL 2>&1
 ) else (
     echo Starting the database.
-    pg_ctl -D database/ -l database/logfile.txt start
+    REM This process must be detached or else CTRL+C from within another program terminates postgres
+    start /b pg_ctl -D database/ -l database/logfile.txt start > NUL 2>&1
 )
-
-endlocal
