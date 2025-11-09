b2d43161abdc42bc27068d89591bf0d47903be70
FriskIsGit <friskisgit@users.noreply.github.com>
2025-11-09 21:42:52 +0100

    Fix scratch->demo

--- web/js/scratch.js
+++ web/js/scratch.js
@@ -29,12 +29,12 @@ function main() {
     //let track = "https://ftp.halifax.rwth-aachen.de/blender/demo/movies/ToS/ToS-4k-1920.mov";
     //let track = "https://test-streams.mux.dev/x36xhzz/url_6/193039199_mp4_h264_aac_hq_7.m3u8"
     let track = "https://video.blender.org/static/web-videos/264ff760-803e-430e-8d81-15648e904183-720.mp4";
-    // let track = "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_640x360.m4v";
+    //let track = "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_640x360.m4v";
     player.setVideoTrack(track);
     player.setTitle("Agent327");
-    player.addSubtitle("media/subs/Tears.of.Steel.2012.vtt");
-    player.addSubtitle("media/subs/Agent327.srt");
-    player.addSubtitle("media/subs/oneline.srt");
+    player.addSubtitle("content/media/subs/Tears.of.Steel.2012.vtt");
+    player.addSubtitle("content/media/subs/Agent327.srt");
+    player.addSubtitle("content/media/subs/oneline.srt");
     player.setVolume(0.01);
     player.onControlsPlay(() => {
         player.setToast("User clicked play.");
@@ -65,6 +65,8 @@ function main() {
         // https://developer.mozilla.org/en-US/docs/Web/API/MediaError
         console.log(error.code, "-", error.message);
     })
+    // Expose player for debugging
+    window.player = player;
 }
 
 main();
