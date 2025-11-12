1eccaa09a4c7a9d143ee0bf50ed6db5e613c4c1a
FriskIsGit <friskisgit@users.noreply.github.com>
2025-11-12 21:46:38 +0100

    Increase minimum segment length to 38ms

--- src/data.go
+++ src/data.go
@@ -68,7 +68,7 @@ const MEDIA_INIT_SECTION = "mis.key"
 const MAX_PLAYLIST_DEPTH = 2
 const MAX_CHUNK_NAME_LENGTH = 26
 const MAX_PLAYLIST_DURATION_SECONDS = 86400 // 24hours
-const MIN_SEGMENT_LENGTH = 0.035            // 35ms
+const MIN_SEGMENT_LENGTH = 0.038            // 38ms
 
 const M3U8_CONTENT_TYPE = "application/vnd.apple.mpegurl"
 const MAX_MESSAGE_CHARACTERS = 1000
